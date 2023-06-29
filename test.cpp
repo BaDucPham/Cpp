e867cb8e7e9cd3ecbd9a1a76567d6385326ec01d
Ba Duc Pham <phambaduc1802@gmail.com>
2023-06-29 23:29:46 +0200

    update

--- test.cpp
+++ test.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-    string fname,lname;
+    string fname, lname;
     cout<<"Enter your first name: ";
     cin>>fname;
     cout<<"Enter your last name: ";
