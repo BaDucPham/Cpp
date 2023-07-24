cb5c288c013f2139731b4694e01cc233b7e97872
Ba Duc Pham <pbduc@math.ac.vn>
2023-07-25 00:56:49 +0200

    update

--- test.cpp
+++ test.cpp
@@ -1,5 +1,10 @@
 #include<iostream>
 using namespace std;
+
+//mô tả chương trình
+//nhập vào họ và tên
+//in ra họ và tên
+
 int main(){
     string fname, lname;
     cout<<"Enter your first name: ";
@@ -8,6 +13,5 @@ int main(){
     cin>>lname;
     cout<<"Your full name is "<<fname<<" "<<lname << "\n";
     return 0;
-    // q: How to create an array
 
 }
